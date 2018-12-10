a9a21289e071aa638de1dd45742bc1df945bed57
Taylor <tbuchhei@gmail.com>
2018-12-09 17:30:12 -0800

    linting code

--- Paul Quillen Website/js/gallery.js
+++ Paul Quillen Website/js/gallery.js
@@ -1,14 +1,18 @@
+/*holds the images in the gallery. Try to follow format below. Place image in gallery folder
+then update this array.*/
 const pics = [
     /*"../img/gallery/gallery_live.jpg",
     "../img/gallery/gallery_stage.jpg",
+    "../img/gallery/gallery_stage_all.jpg",
     "../img/gallery/gallery_playing.jpg",
     "../img/gallery/gallery_portrait.jpg",*/
     "https://github.com/7aylor/7aylor.github.io/blob/master/Paul%20Quillen%20Website/img/gallery/gallery_live.jpg?raw=true",
     "https://github.com/7aylor/7aylor.github.io/blob/master/Paul%20Quillen%20Website/img/gallery/gallery_playing.jpg?raw=true",
     "https://github.com/7aylor/7aylor.github.io/blob/master/Paul%20Quillen%20Website/img/gallery/gallery_portrait.jpg?raw=true",
     "https://github.com/7aylor/7aylor.github.io/blob/master/Paul%20Quillen%20Website/img/gallery/gallery_stage.jpg?raw=true"    
-]
+];
 
+//get elements on the page
 const exitBtn = document.getElementById("exit");
 const gallery = document.getElementById("gallery");
 const leftArr = document.getElementById("left-arrow");
@@ -16,26 +20,37 @@ const rightArr = document.getElementById("right-arrow");
 const counter = document.getElementById("counter");
 const img = document.getElementById("gallery-img");
 
-let currImg = 0;
-let currY = 0;
+let currImg = 0; //keeps track of current img in the array being displayed
+let currY = 0; //used to lock y position when gallery is active
 let canClickArrow = false;
 
+//called when X is pressed to exit gallery
 function exitGallery(){
-    window.removeEventListener('scroll', noscroll);
-    gallery.style.display = 'none';
+    "use strict";
+    window.removeEventListener("scroll", noscroll);
+    upBtn.style.display = "block";
+    downBtn.style.display = "block";
+    gallery.style.display = "none";
 }
 
+//called from the gallery link in media section
 function openGallery(){
-    gallery.style.display = 'block';
+    "use strict";
+    gallery.style.display = "block";
     img.src = pics[currImg];
     currY = window.scrollY;
-    window.addEventListener('scroll', noscroll);
+    window.addEventListener("scroll", noscroll);
+    upBtn.style.display = "none";
+    downBtn.style.display = "none";
+
     //wait for image to load before allowing another click,
     //not using arrow function because ie doesn't support it
-    img.onload = function() { updateCounterAndCanClick() };
+    img.onload = function() { updateCounterAndCanClick(); }
 }
 
+//called from prev arrow being clicked. Decrements image counter and updates displaying image
 function prevPic(){
+    "use strict";
     if(canClickArrow == true){
         canClickArrow = false;
         currImg--;
@@ -45,12 +60,15 @@ function prevPic(){
         }
 
         img.src = pics[currImg];
+
         //wait for image to load before allowing another click
-        img.onload = function() { updateCounterAndCanClick() };
+        img.onload = function() { updateCounterAndCanClick(); }
     }
 }
 
+//called from next arrow being clicked. Increments image counter and updates displaying image
 function nextPic(){
+    "use strict";
     if(canClickArrow == true){
         canClickArrow = false;
         currImg++;
@@ -60,17 +78,22 @@ function nextPic(){
         }
     
         img.src = pics[currImg];
+
         //wait for image to load before allowing another click
-        img.onload = function() { updateCounterAndCanClick() };
+        img.onload = function() { updateCounterAndCanClick(); }
     }
 }
 
+//updates current image counter and condition to allow clicking next/prev arrows
 function updateCounterAndCanClick(){
+    "use strict";
     counter.innerHTML = currImg + 1 + " / " + pics.length;
     canClickArrow = true;
 }
 
+//locks Y scrolling
 function noscroll() {
+    "use strict";
     window.scrollTo(0, currY);
 }
 
--- Paul Quillen Website/js/scrolling.js
+++ Paul Quillen Website/js/scrolling.js
@@ -1,9 +1,11 @@
+//get elements in html and store in vars
 const upBtn = document.getElementById("up-btn");
 const downBtn = document.getElementById("down-btn");
 const body = document.getElementsByTagName("body");
 const music = document.getElementById("bandcamp");
-let currSection = 0;
-let sections = [
+
+//array to hold page sections, needs to be in order they appear in html
+const sections = [
     "banner",
     "about",
     "music",
@@ -16,28 +18,34 @@ let sections = [
 const isChrome = !!window.chrome && !!window.chrome.webstore;
 
 // Firefox 1.0+
-const isFirefox = typeof InstallTrigger !== 'undefined';
+const isFirefox = typeof InstallTrigger !== "undefined";
 
-//used to enable scrolling buttons if on chrome or firefox
-let enableScrolling = false;
 
+let enableScrolling = false; //used to enable scrolling buttons if on chrome or firefox
+let currSection = 0; //keeps track of which section we are in related to the array
+
+//initially call to hide/show scroll buttons on proper browsers
 showScrollButtons();
 
+//when page loads, check if firefox/chrome to enable buttons when ready to scroll
 window.onload = function () {
     if(isChrome || isFirefox){
         enableScrolling = true;
-        upBtn.parentNode.style.display = 'block';
-        downBtn.parentNode.style.display = 'block';
-        window.scrollTo(0, 0);
+        upBtn.parentNode.style.display = "block";
+        downBtn.parentNode.style.display = "block";
     }
 };
 
+//when user scrolls mouse wheel, check which buttons should show
 window.onscroll = function() {
     if(enableScrolling){
+        getClosestSection();
         showScrollButtons();
+        console.log("scrolling");
     }
 };
 
+//determines which scroll buttons should display on the page, top, bottom, or both depending on screen position Y
 function showScrollButtons(){
     if(window.scrollY == 0){
         upBtn.style.display = "none";
@@ -53,11 +61,13 @@ function showScrollButtons(){
     }
 }
 
+//called from navbar buttons
 function scrollDown(id){
     document.getElementById(id.toString()).scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
     updateCurrSection(id);
 }
 
+//called from down button onclick
 function scrollDownOne(){
     if(currSection < sections.length - 1){
         currSection++;
@@ -65,6 +75,7 @@ function scrollDownOne(){
     document.getElementById(sections[currSection]).scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
 }
 
+//called from up button on click
 function scrollUpOne(){
     if(currSection > 0){
         getClosestSection(true);   
@@ -72,6 +83,7 @@ function scrollUpOne(){
     document.getElementById(sections[currSection]).scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
 }
 
+//updates currSection var to the section in the array
 function updateCurrSection(id){
     for(let i = 0; i < sections.length; i++){
         if(sections[i] == id){
@@ -81,11 +93,7 @@ function updateCurrSection(id){
     }
 }
 
-window.addEventListener("scroll", function() {
-    getClosestSection();
-});
-
-
+//get closest section based on y position
 function getClosestSection(up){
     let yPos = window.scrollY;
     let shortest = document.body.clientHeight;
